c6471e275c80a8f6707a148dc64b6a77bd4a5f0c
uesleidcn <ueslei@ztrax.com.br>
2025-04-16 21:04:30 -0300

    start users mdule

--- src/frameworks/config/environment.ts
+++ src/frameworks/config/environment.ts
@@ -5,6 +5,6 @@ dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 export default {
   port: process.env.PORT || 3000,
-  mongodbUri: process.env.MONGODB_URI || 'mongodb://localhost:27017/my-med-db',
+  mongodbUri: process.env.MONGODB_URI || 'mongodb+srv://subsueslei:xoZDNNQohVY64srJ@cluster0.bhrqape.mongodb.net/',
   nodeEnv: process.env.NODE_ENV || 'development',
 };
