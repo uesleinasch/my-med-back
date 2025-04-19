15e37ec7e60cde08966ede7d5ee25bfef9ac8f6b
uesleidcn <ueslei@ztrax.com.br>
2025-04-19 16:09:52 -0300

    remove all users relations

--- src/modules/users/domain/user.entity.ts
+++ src/modules/users/domain/user.entity.ts
@@ -6,34 +6,6 @@ export interface User {
   email: string;
   password: string;
   role: string;
-  relations: Relation[];
-  medicalProfile: MedicalProfile;
   createdAt?: Date;
   updatedAt?: Date;
-}
-
-export interface Relation {
-  userId: string | ObjectId;
-  type: string;
-  since: Date;
-}
-
-export interface MedicalProfile {
-  allergies: Allergy[];
-  conditions: Condition[];
-}
-
-export interface Allergy {
-  allergyId: string | ObjectId;
-  relationshipType: 'self' | 'dependent';
-  diagnosedAt: Date;
-  severity: string;
-  notes?: string;
-}
-
-export interface Condition {
-  conditionId: string | ObjectId;
-  relationshipType: 'self' | 'dependent';
-  diagnosedAt: Date;
-  status: string;
 }
--- src/modules/users/infrastructure/models/user.model.ts
+++ src/modules/users/infrastructure/models/user.model.ts
@@ -6,73 +6,6 @@ export interface UserDocument extends User, Document {
   id: string;
 }
 
-// Schema para relações
-const relationSchema = new Schema({
-  userId: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    ref: 'User'
-  },
-  type: {
-    type: String,
-    required: true
-  },
-  since: {
-    type: Date,
-    default: Date.now
-  }
-});
-
-// Schema para alergias
-const allergySchema = new Schema({
-  allergyId: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    ref: 'Allergy'
-  },
-  relationshipType: {
-    type: String,
-    enum: ['self', 'dependent'],
-    required: true
-  },
-  diagnosedAt: {
-    type: Date,
-    required: true
-  },
-  severity: {
-    type: String,
-    required: true
-  },
-  notes: String
-});
-
-// Schema para condições médicas
-const conditionSchema = new Schema({
-  conditionId: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    // ref: 'Condition' // Será implementado quando tivermos o modelo de condições
-  },
-  relationshipType: {
-    type: String,
-    enum: ['self', 'dependent'],
-    required: true
-  },
-  diagnosedAt: {
-    type: Date,
-    required: true
-  },
-  status: {
-    type: String,
-    required: true
-  }
-});
-
-const medicalProfileSchema = new Schema({
-  allergies: [allergySchema],
-  conditions: [conditionSchema]
-});
-
 const userSchema = new Schema(
   {
     name: {
@@ -92,14 +25,6 @@ const userSchema = new Schema(
       type: String,
       required: true,
       default: 'user'
-    },
-    relations: [relationSchema],
-    medicalProfile: {
-      type: medicalProfileSchema,
-      default: {
-        allergies: [],
-        conditions: []
-      }
     }
   },
   {
